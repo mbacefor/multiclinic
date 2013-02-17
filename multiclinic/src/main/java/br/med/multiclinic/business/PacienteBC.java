f0744c4a275a36491309f2684153fff65ed81d25
mbacefor <mbacefor@Marcelo-PC>
2013-02-17 15:02:13 -0300

    Fonetização correta

--- multiclinic/src/main/java/br/med/multiclinic/business/PacienteBC.java
+++ multiclinic/src/main/java/br/med/multiclinic/business/PacienteBC.java
@@ -3,10 +3,13 @@
  */
 package br.med.multiclinic.business;
 
+import javax.inject.Inject;
+
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.med.multiclinic.domain.Paciente;
 import br.med.multiclinic.domain.Usuario;
 import br.med.multiclinic.persistence.PacienteDAO;
+import br.med.multiclinic.util.Fonetizador;
 
 /**
  * @author fa7bd
@@ -18,12 +21,12 @@ public class PacienteBC extends GenericoBC<Paciente, Long, PacienteDAO> {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	public void salvar(Paciente paciente, Usuario usuarioLogado) {
+	@Inject
+	Fonetizador fonetizador;
 
-		//Fonetizador fonetizador = new Fonetizador();
-		paciente.setNomeFonetico((paciente
-				.getNome()));
+	public void salvar(Paciente paciente, Usuario usuarioLogado) {
+		paciente.setNomeFonetico(fonetizador.fornetizarComDicionario((paciente
+				.getNome())));
 
 		super.salvar(paciente, usuarioLogado);
 	}
