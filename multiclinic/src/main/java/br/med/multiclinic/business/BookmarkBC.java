79ad9d67ac3d424136f2b2b06fff6d84f070d10e
mbacefor <mbacefor@gmail.com>
2015-03-21 12:16:06 -0300

    Ajuste do bc

--- multiclinic/src/main/java/br/med/multiclinic/business/BookmarkBC.java
+++ multiclinic/src/main/java/br/med/multiclinic/business/BookmarkBC.java
@@ -10,24 +10,33 @@ import br.med.multiclinic.persistence.BookmarkDAO;
 
 @BusinessController
 public class BookmarkBC extends DelegateCrud<Bookmark, Long, BookmarkDAO> {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Startup
 	@Transactional
 	public void load() {
 		if (findAll().isEmpty()) {
-			insert(new Bookmark("Demoiselle Portal", "http://www.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Demoiselle SourceForge", "http://sf.net/projects/demoiselle"));
-			insert(new Bookmark("Twitter", "http://twitter.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Blog", "http://blog.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Wiki", "http://wiki.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Bug Tracking", "http://tracker.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Forum", "http://forum.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Demoiselle Portal",
+					"http://www.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Demoiselle SourceForge",
+					"http://sf.net/projects/demoiselle"));
+			insert(new Bookmark("Twitter",
+					"http://twitter.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Blog",
+					"http://blog.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Wiki",
+					"http://wiki.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Bug Tracking",
+					"http://tracker.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Forum",
+					"http://forum.frameworkdemoiselle.gov.br"));
 			insert(new Bookmark("SVN", "http://svn.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Maven", "http://repository.frameworkdemoiselle.gov.br"));
-			insert(new Bookmark("Downloads", "http://download.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Maven",
+					"http://repository.frameworkdemoiselle.gov.br"));
+			insert(new Bookmark("Downloads",
+					"http://download.frameworkdemoiselle.gov.br"));
 		}
 	}
-	
+
 }
